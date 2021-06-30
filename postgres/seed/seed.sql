54b993a5b9e33aac52da9e958eeea22c4b7de6d3
Marrick Somal <marrick.somal@gmail.com>
2021-06-30 16:34:45 +0100

    feat: allow age and pet fields to be updated

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,4 +1,4 @@
 -- Seed data with a fake user for testing (Password: plainSnow41)
 
-insert into users (name, email, entries, joined) values ('barry', 'barry@h.com', 5, '2021-01-01');
+insert into users (name, age, pet, email, entries, joined) values ('barry', 33, 'racoon', 'barry@h.com', 5, '2021-01-01');
 insert into login (hash, email) values ('$2a$10$1pisEFpc6SiFyzq.hBD7F.Y3bDl5ew8xThwNZM5XLIXuubSZ8xgH.', 'barry@h.com');
