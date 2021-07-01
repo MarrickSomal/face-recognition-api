288fba5a0d8228804bdb8b51fbdb46c561cf643c
Marrick Somal <marrick.somal@gmail.com>
2021-07-01 12:33:05 +0100

    feat: register endpoint token flow

--- controllers/register.js
+++ controllers/register.js
@@ -1,36 +1,60 @@
+const jwt = require('jsonwebtoken');
+
+// Redis Setup
+const redis = require('redis');
+
+// You will want to update your host to the proper address in production
+const redisClient = redis.createClient(process.env.REDIS_URI);
+
+const registerToken = (username) => {
+  const jwtPayload = { username };
+  return jwt.sign(jwtPayload, 'JWT_SECRET_KEY', { expiresIn: '2 days'});
+};
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = registerToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return token
+    })
+    .catch(console.log);
+};
+
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
+   db.transaction(trx => {
       trx.insert({
         hash: hash,
-        email: email
+        email: email,
       })
       .into('login')
       .returning('email')
-      .then(loginEmail => {
+      .then((loginEmail) => {
         return trx('users')
           .returning('*')
           .insert({
             email: loginEmail[0],
             name: name,
-            joined: new Date()
+            joined: new Date(),
           })
-          .then(user => {
-            res.json(user[0]);
+          .then(async (user) => {
+            const token = await createSession(user[0])
+            res.json({ user: user[0], token: token })
           })
+          .then(trx.commit)
       })
-      .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json(err))
+    .catch(err => err)
 }
 
 module.exports = {
-  handleRegister: handleRegister
+  handleRegister: handleRegister,
 };
-
-
